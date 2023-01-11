65b8a3d993d2f84ebd9d72a266b820dbd1953f07
aAndyeE <andy.emich@docc.techstarter.de>
2023-01-11 15:49:12 +0100

    neu

---
+++ auslesen.py
@@ -0,0 +1,6 @@
+import json
+# Öffne die Datei
+with open('tsjsonneu/accounts.json', 'r') as f:
+    # Lade die JSON-Daten aus der Datei
+    data = json.load(f)
+    print(json.dumps(data, indent=4))
