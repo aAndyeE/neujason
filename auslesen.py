b0f45b11bceed1e7896e8608b77cdfee14b5c945
aAndyeE <andy.emich@docc.techstarter.de>
2023-01-19 14:11:03 +0100

    weiter

--- auslesen.py
+++ auslesen.py
@@ -5,20 +5,21 @@ with open('tsjsonneu/accounts.json', 'r') as f:
     data = json.load(f)
     #print(json.dumps(data, indent=4))
 
-eingabe = input('Was möchtest du tun?\ngib ;alter; ein für personen über 25 Jahre\nfür den aktuellen Kontostand gib ;kontostand; ein')
+eingabe = input('Was möchtest du tun?\ngib ;alter; ein für personen über 25 Jahre\nfür den aktuellen Kontostand gib ;kontostand; ein ')
 
 
 if eingabe == 'alter':
 
     for alter in data:
         if alter['age'] > 25:
-            print(json.dumps(alter, indent=4))
+            #print(json.dumps(alter, indent=4))
+            print('Name: ', alter['name'], alter['age'])
         
-if eingabe == "kontostand":
+elif eingabe == "kontostand":
     summe = 0
     for accounts in data:
         pruf = accounts["balance"].replace(",","")
         pruf = float(pruf)
         if summe < pruf:
                 summe = pruf
-    print(summe)
+    print('Den hoesten Kontostand hat: ',accounts['name'],'mit ',summe)
